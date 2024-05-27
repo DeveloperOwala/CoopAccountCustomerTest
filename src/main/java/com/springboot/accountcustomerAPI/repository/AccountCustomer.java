b09c850781e8bda4ba57cca0fdd6c738644afcd1
Erick <otienoerick46@gmail.com>
2024-05-27 15:34:31 +0300

    account customers

--- src/main/java/com/springboot/accountcustomerAPI/repository/AccountCustomer.java
+++ src/main/java/com/springboot/accountcustomerAPI/repository/AccountCustomer.java
@@ -1,13 +1,13 @@
 package com.springboot.accountcustomerAPI.repository;
 
-import com.springboot.accountcustomerAPI.model.Employee;
+import com.springboot.accountcustomerAPI.model.AccountCustomer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AccountCustomerRepository  extends JpaRepository<Employee, Long> {
+public interface AccountCustomerRepository  extends JpaRepository<AccountCustomer, Long> {
     void deleteCustomerById(Long id);
 
     AccountCustomer getCustomerById(Long id);
