507f3e989eb169a5a108b958306e9bd39ca57656
Erick <otienoerick46@gmail.com>
2024-05-27 15:40:54 +0300

    account customers

--- src/test/java/com/springboot/accountCustomerAPI/accountCustomerAPIApplicationTests.java
+++ src/test/java/com/springboot/accountCustomerAPI/accountCustomerAPIApplicationTests.java
@@ -1,10 +1,10 @@
-package com.springboot.employeeAPI;
+package com.springboot.accountCustomerAPI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmployeeApiApplicationTests {
+class AccountCustomerApiApplicationTests {
 
 	@Test
 	void contextLoads() {
